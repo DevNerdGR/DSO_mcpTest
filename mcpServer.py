3c58544edff425c7270c3504cacbadae40c25d18
Duck <duck@TewGR.>
2025-08-12 23:50:48 +0800

    Added function that makes external API call

--- mcpServer.py
+++ mcpServer.py
@@ -1,5 +1,15 @@
 from fastmcp import FastMCP
 import random
+import requests
+import json
+
+"""
+README: To add this to Claude desktop, add the follwing to the config file:
+"greeter": {
+      "command": "python",
+      "args": ["C:/Users/GR/Desktop/Horizon/Research_DSO/mcp ahh/MCP_TEST/mcpServer.py"]
+}
+"""
 
 mcp = FastMCP("My MCP Server")
 
@@ -11,6 +21,12 @@ def greet(name : str) -> str:
 def rollDice(numberOfSides : int):
     """Rolls a x sided dice, with x being specified by the function parameter."""
     return random.randint(1, numberOfSides)
+
+@mcp.tool
+def getNerdyJoke() -> str:
+    """This function calls on an external API and returns a nerdy joke as a string."""
+    response = requests.get("https://geek-jokes.sameerkumar.website/api?format=json").text
+    return (json.loads(response))["joke"]
 
 if __name__ == "__main__":
     mcp.run()
